90022856a3669c22d19bf1aaf253b0eb0346c019
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 21:32:48 +0300

    Create variables.tf

---
+++ 3modules2/network/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+    type = string
+    description = "project id"
+    default = "civic-access-350318"
+
+  
+}
+
+variable "network_name" {
+type = string
+description = "name of the network"
+}
