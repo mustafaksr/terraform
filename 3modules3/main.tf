79e1d468ecd62db1f45ea9f44751e28d688ede09
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 22:03:22 +0300

    Update main.tf

--- 3modules3/main.tf
+++ 3modules3/main.tf
@@ -1,8 +1,19 @@
 provider "google" {
-    region = "us-central-1"
-  
+  region = "us-central-1"
+
 }
-module "environments" {
-    source = "./environments"
+
+module "environments_dev" {
+  source = "./environments/dev"
+
+}
+
+module "environments_prod" {
+  source = "./environments/production"
+
+}
+
+module "environments_test" {
+  source = "./environments/test"
 
 }
