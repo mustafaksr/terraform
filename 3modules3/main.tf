482b4f15f7b5dd5b87b291b626687e14ee4bcfd5
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 22:11:24 +0300

    Update main.tf

--- 3modules3/main.tf
+++ 3modules3/main.tf
@@ -5,15 +5,18 @@ provider "google" {
 
 module "environments_dev" {
   source = "./environments/dev"
+  vm_count = var.vm_count_dev
 
 }
 
 module "environments_prod" {
   source = "./environments/production"
+  vm_count = var.vm_count_prod
 
 }
 
 module "environments_test" {
   source = "./environments/test"
+  vm_count = var.vm_count_test
 
 }
