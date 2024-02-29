79c54993536c41765fca783f59060cea4a133959
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 21:33:53 +0300

    Create variables.tf

---
+++ inf1/modules/storage/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+ default = "us-east1"
+}
+variable "zone" {
+ default = "us-east1-d"
+}
+variable "project_id" {
+ type = string
+ description = "Enter your cloud project id"
+ default = "civic-access-350318"
+}
