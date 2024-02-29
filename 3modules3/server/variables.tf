f0a49731b1e5bd563bb79a458480230cf6d173e2
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 21:33:37 +0300

    Create variables.tf

---
+++ 3modules3/server/variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+    type = string
+    description = "project id"
+    default = "civic-access-350318"
+}
+
+variable "instance_name" {
+    description = "vm name"
+}
+variable "instance_zone" {
+    type = string
+    description = "VM zone"
+  
+}
+
+variable "instance_type" {
+    default = "e2-medium"
+  
+}
+
+variable "vm_count" {
+    type = number
+  
+}
