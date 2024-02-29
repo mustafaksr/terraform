394410442326d824987c550a588f005191eafc54
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 21:33:30 +0300

    Create variable.tf

---
+++ 1modules_infra/infra/instance/variable.tf
@@ -0,0 +1,10 @@
+variable "instance_name" {}
+variable "instance_zone" {}
+variable "instance_type" {
+  default = "e2-micro"
+  }
+variable "instance_network" {}
+variable "project" {
+  default = "civic-access-350318"
+  
+}
