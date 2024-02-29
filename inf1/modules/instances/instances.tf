0200cde80fe8ba0b09ef8d0e40664a81888a8641
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-02-29 21:33:41 +0300

    Create instances.tf

---
+++ inf1/modules/instances/instances.tf
@@ -0,0 +1,62 @@
+resource "google_compute_instance" "tf-instance-1"{
+  name         = "tf-instance-1"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = var.tf_vpc_network
+     subnetwork = "subnet-01"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
+
+resource "google_compute_instance" "tf-instance-2"{
+  name         = "tf-instance-2"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = var.tf_vpc_network
+     subnetwork = "subnet-02"
+  }
+
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
+
+resource "google_compute_instance" "tf-instance-610252" {
+  name         = "tf-instance-610252"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+ network = "default"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
